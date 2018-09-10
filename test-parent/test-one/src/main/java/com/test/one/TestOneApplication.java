17aed03ab5267886dcc726354c92c870011610d3
wongClub <wangi0214@163.com>
2018-09-10 12:46:58 +0800

    项目提交

---
+++ test-parent/test-one/src/main/java/com/test/one/TestOneApplication.java
@@ -0,0 +1,12 @@
+package com.test.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOneApplication.class, args);
+	}
+}
---
+++ test-parent/test-two/src/main/java/com/test/two/testtwo/TestTwoApplication.java
@@ -0,0 +1,12 @@
+package com.test.two.testtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTwoApplication.class, args);
+	}
+}
